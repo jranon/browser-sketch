df2cf22ba707a9af24be5ca4adccb7caaf1a841f
jranon <jesse.ranon@gmail.com>
2015-06-03 23:10:46 -0700

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,7 @@
 var grid = function() { //generate the sketch pad grid
 	var r = prompt("How many pixels do you want in your grid?", "Please enter a number."); //ask user for the number of pixels.
 		if (isNaN(r) || r > 128) {
-			r = prompt("Please enter a number between 10 and 64");
+			r = prompt("Please enter a number between 10 and 128");
 		}
 	var sq = parseInt(r);
 	var hw = 900/sq; //calculate pixel size against un-changing container div size.
